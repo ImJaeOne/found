256f7b01e858a70277af9759113076b0c188526e
ImJaeOne <dlawi0108@naver.com>
2025-03-21 09:51:24 +0900

    🎉 Add: supabase 연동

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,8 @@
-const Home = () => {
+import { supabase } from '@/services/supabaseClient';
+
+const Home = async () => {
+  const data = await supabase.from('test').select('*');
+  console.log(data);
   return <div className="text-sub1 text-xl">1석이조</div>;
 };
 
