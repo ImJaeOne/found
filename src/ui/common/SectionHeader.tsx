c0f865dcf58647dfb6dd26dd47b7825e6a36aeae
ImJaeOne <dlawi0108@naver.com>
2025-03-21 20:22:14 +0900

    🎉 Add: SectionHeader Component #12

--- src/ui/common/SectionHeader.tsx
+++ src/ui/common/SectionHeader.tsx
@@ -29,7 +29,7 @@ const SectionHeader = ({ selected, setSelected }: SectionHeaderProps) => {
   );
 
   return (
-    <div className="w-full border-b border-medium-gray flex justify-end">
+    <div className="w-full border-b border-medium-gray flex justify-end cursor-pointer">
       {path === PATH.CHATTING && (
         <div className="text-main1 text-title-sm font-bold border-b-2 border-main1 px-4 py-2">
           CHAT
