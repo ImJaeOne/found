2c79ab8bb960e305ac15e06a76c2f14f3d585402
K-jisu <wltn7star@naver.com>
2025-03-21 23:13:16 +0900

    ✨ update : 카테고리 상수 추가#10

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -6,3 +6,12 @@ export const PATH = {
   MATELIST: '/matelist',
   CHATTING: '/chatting',
 };
+
+export const CATEGORIES = {
+  RUNNING: "런닝",
+  BADMINTON: "배드민턴",
+  WEIGHT: "웨이트",
+  BASKETBALL: "농구",
+  FUTSAL: "풋살",
+  BOXING: "복싱",
+}
