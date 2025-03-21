7e4025e4da67dc22db4482c522c4ae874f212dce
miing_gaeng <pem4134@naver.com>
2025-03-22 00:08:01 +0900

    🎉 Add: hookform/resolvers, supabase/ssr 라이브러리 설치 #4

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,9 +1,19 @@
-import AppointmentCard from './(components)/AppointmentCard';
+import UserProfile from '@/ui/common/UserProfile';
+import Appointments from './(components)/Appointments';
 
 const MyPage = () => {
+  const user = {
+    nick_name: 'Nickname',
+    bio: 'bio',
+    categories: ['런닝', '배드민턴', '축구'],
+  };
+
   return (
-    <div>
-      <AppointmentCard />
+    <div className="flex justify-center gap-24">
+      <UserProfile user={user} edit={true}>
+        <div>children</div>
+      </UserProfile>
+      <Appointments />
     </div>
   );
 };
