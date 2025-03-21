1ed61c428fad890c0835285b3d3f38b8f41f5998
ImJaeOne <dlawi0108@naver.com>
2025-03-21 10:00:26 +0900

    🧹 Cleanup: supabase 확인 코드 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,4 @@
-import { supabase } from '@/services/supabaseClient';
-
 const Home = async () => {
-  const data = await supabase.from('test').select('*');
-  console.log(data);
   return <div className="text-sub1 text-xl">1석이조</div>;
 };
 
