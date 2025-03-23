c557a5c45c9685e14909de7110b5fb7f9fb98883
ImJaeOne <dlawi0108@naver.com>
2025-03-23 19:12:51 +0900

    🎉 Add: useFindAddress hook 구현 #22

---
+++ src/hooks/useFindAddress.ts
@@ -0,0 +1,46 @@
+import { useEffect, useState } from 'react';
+
+declare global {
+  interface Window {
+    daum: any;
+  }
+}
+
+export const useFindAddess = () => {
+  const [roadAddress, setRoadAddress] = useState('');
+
+  const [isScriptLoaded, setIsScriptLoaded] = useState(false);
+
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src =
+      'https://t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js';
+    script.async = true;
+    script.onload = () => setIsScriptLoaded(true);
+    document.body.appendChild(script);
+  }, []);
+
+  const handlePostcodeSearch = () => {
+    new window.daum.Postcode({
+      oncomplete: (data: any) => {
+        let extraRoadAddr = '';
+
+        if (data.bname !== '' && /[동|로|가]$/g.test(data.bname)) {
+          extraRoadAddr += data.bname;
+        }
+        if (data.buildingName !== '' && data.apartment === 'Y') {
+          extraRoadAddr += extraRoadAddr
+            ? `, ${data.buildingName}`
+            : data.buildingName;
+        }
+        if (extraRoadAddr !== '') {
+          extraRoadAddr = ` (${extraRoadAddr})`;
+        }
+
+        setRoadAddress(data.roadAddress);
+      },
+    }).open();
+  };
+
+  return { roadAddress, handlePostcodeSearch };
+};
