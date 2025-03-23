9c69ba8a538de2f78908728bd3153920728402ae
ImJaeOne <dlawi0108@naver.com>
2025-03-23 14:14:17 +0900

    ♻️ Refactor: SectionHeader client -> server

---
+++ src/app/chatting/_components/SectionHeader.tsx
@@ -0,0 +1,11 @@
+const SectionHeader = () => {
+  return (
+    <div className="w-full border-b border-medium-gray flex justify-end">
+      <div className="text-main1 text-title-sm font-bold border-b-2 border-main1 px-4 py-2">
+        CHAT
+      </div>
+    </div>
+  );
+};
+
+export default SectionHeader;
