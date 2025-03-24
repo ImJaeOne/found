e35b9e99d1f252b6689c61d732fc465aaba5baae
Juha Yoon <ijooha16@gmail.com>
2025-03-24 17:06:31 +0900

    🎉 Add: 약속 디테일 관련 컴포넌트 #42

---
+++ src/app/mypage/_components/appointment-detail/DetailContent.tsx
@@ -0,0 +1,12 @@
+const DetailContent = () => {
+  return (
+    <div className="flex w-[280px] flex-col gap-2">
+      <div className="text-main1 text-title-sm font-bold">CONTENTS</div>
+      <div className="text-medium-gray text-sm">
+        어쩌구 저쩌구 준비물 와랄라ㅏ라랄라
+      </div>
+    </div>
+  );
+};
+
+export default DetailContent;
