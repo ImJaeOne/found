89632e4d4765844126c3c75448ff420c7f067149
K-jisu <wltn7star@naver.com>
2025-03-24 16:10:24 +0900

    ✨ Add: prefetching 추가 #24

--- src/app/matelist/page.tsx
+++ src/app/matelist/page.tsx
@@ -1,9 +1,25 @@
+import { getUsers } from '@/services/getServices';
 import UserCardList from './_components/UserCardList';
 
-const MateListPage = () => {
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from '@tanstack/react-query';
+
+const MateListPage = async () => {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ['users'],
+    queryFn: getUsers,
+  });
+
   return (
-    <div className="pt-32">
-      <UserCardList />
+    <div className="">
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <UserCardList />
+      </HydrationBoundary>
     </div>
   );
 };
