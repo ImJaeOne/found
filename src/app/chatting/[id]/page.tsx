30a732001124703d76d4977e0d448557919177e8
ImJaeOne <dlawi0108@naver.com>
2025-03-24 23:46:16 +0900

    🧹 Cleanup: 불필요한 코드 삭제 #35

--- src/app/chatting/[id]/page.tsx
+++ src/app/chatting/[id]/page.tsx
@@ -1,17 +1,7 @@
-import UserProfile from '@/ui/common/UserProfile';
 import Chatting from '../_components/Chatting';
-import Link from 'next/link';
 import SectionHeader from '../_components/SectionHeader';
-import { PATH } from '@/constants/constants';
 import { Params } from '@/types/params';
 import ChattingUserProfile from '../_components/\bChattingUserProfile';
-
-const partner = {
-  user_id: 2,
-  nick_name: '케빈',
-  bio: '운동 좋아하는 26남',
-  categories: ['런닝', '배드민턴', '축구'],
-};
 
 const ChattingPage = async ({ params }: Params) => {
   const chatId = Number(params.id);
