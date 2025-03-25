c0ccd0f87719426a96984990ff6eb021a42e7a60
ImJaeOne <dlawi0108@naver.com>
2025-03-25 14:53:33 +0900

    ✨ Update: zustand user정보를 통해 상대 정보 가져오는 로직 #35

--- src/app/chatting/_components/ChattingUserProfile.tsx
+++ src/app/chatting/_components/ChattingUserProfile.tsx
@@ -2,36 +2,21 @@
 
 import { PATH } from '@/constants/constants';
 import { supabase } from '@/services/supabaseClient';
-import UserProfile from '@/ui/common/UserProfile';
 import { useQuery } from '@tanstack/react-query';
 import Link from 'next/link';
 import React from 'react';
+import UserProfile from './UserProfile';
+import { useAuthStore } from '@/providers/AuthProvider';
+import { UserData } from '@/types/users';
 
 const ChattingUserProfile = ({ chatId }: { chatId: number }) => {
-  const { data: userId } = useQuery({
-    queryKey: ['userId'],
-    queryFn: async () => {
-      const { data: sessionData } = await supabase.auth.getSession();
-      const userSessionId = sessionData?.session?.user.id;
-      if (!userSessionId) return null;
-
-      const { data: userData } = await supabase
-        .from('users')
-        .select('id')
-        .eq('user_id', userSessionId)
-        .single();
-
-      return userData?.id || null;
-    },
-    staleTime: 1000 * 60,
-  });
-
+  const { user } = useAuthStore((state) => state);
   const {
     data: partner,
     isPending,
     isError,
   } = useQuery({
-    queryKey: ['chatPartner', chatId, userId],
+    queryKey: ['userInfo', user?.id],
     queryFn: async () => {
       const { data, error } = await supabase
         .from('chat_rooms')
@@ -42,43 +27,43 @@ const ChattingUserProfile = ({ chatId }: { chatId: number }) => {
       if (error) throw new Error(error.message);
 
       const otherUserId =
-        data.user1_id === userId ? data.user2_id : data.user1_id;
+        data.user1_id === user?.id ? data.user2_id : data.user1_id;
 
       const { data: otherUser, error: userError } = await supabase
         .from('users')
-        .select('id, nickname, profile, bio, user_categories(category)')
+        .select('*, user_categories(category)')
         .eq('id', otherUserId)
         .single();
 
       if (userError) throw new Error(userError.message);
 
-      const categories = otherUser.user_categories.map((item) => item.category);
+      const categories = otherUser.user_categories.map(
+        (item: { category: string }) => item.category,
+      );
 
-      const otherUserInfo: User = { ...otherUser, categories };
-
+      const otherUserInfo: UserData = {
+        ...otherUser,
+        categories,
+      };
       return otherUserInfo;
     },
-    enabled: !!chatId && !!userId,
+    enabled: !!chatId && !!user?.id,
   });
+  if (!user) return <div>상대 정보를 가져오는 중...</div>;
   if (isPending) return <div>Loading...</div>;
   if (isError) return <div>Error...</div>;
 
   return (
-    <UserProfile user={partner} edit={false}>
-      <div>
-        <Link href={PATH.APPOINTMENT}>약속 요청하기</Link>
+    <div className="flex flex-col justify-between shadow-md p-4 rounded-md">
+      <UserProfile user={partner} />
+      <div className="flex flex-col gap-4 p-3 rounded-md items-center bg-main2">
+        <Link href={PATH.APPOINTMENT} className="">
+          약속 요청하기
+        </Link>
         <Link href={PATH.APPOINTMENT}>약속 확인하기</Link>
       </div>
-    </UserProfile>
+    </div>
   );
 };
 
 export default ChattingUserProfile;
-
-type User = {
-  id: number;
-  nickname: string;
-  bio: string;
-  profile: string;
-  categories: string[];
-};
