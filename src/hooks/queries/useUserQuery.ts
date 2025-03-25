47b5ed65f264e0cae69a1404bedf8b802906954d
ImJaeOne <dlawi0108@naver.com>
2025-03-25 17:16:04 +0900

    ♻️ Refactor: useUserQuery hook 분리 #64

--- src/hooks/queries/useUserQuery.ts
+++ src/hooks/queries/useUserQuery.ts
@@ -1,10 +1,11 @@
+import { QUERY_KEY } from '@/constants/constants';
 import { fetchChatPartner } from '@/services/usersServices';
 import { UserData } from '@/types/users';
 import { useQuery } from '@tanstack/react-query';
 
 export const useGetChatPartner = (chatId: number, user: UserData | null) => {
   return useQuery({
-    queryKey: ['userInfo', user?.id],
+    queryKey: [QUERY_KEY.CHAT_PARTNER, user?.id],
     queryFn: () => fetchChatPartner(chatId, user!.id),
     enabled: !!chatId && !!user?.id,
   });
