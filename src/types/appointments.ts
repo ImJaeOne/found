ff4a2f41318e0c4ca6f7249d634980a43355e9aa
miing_gaeng <pem4134@naver.com>
2025-03-26 01:04:34 +0900

    🎉 Add: Appoinment 데이터 supabase 테이블에 추가 로직 #72

--- src/types/appointments.ts
+++ src/types/appointments.ts
@@ -1,3 +1,12 @@
+export type Appointment = {
+  title: string;
+  place: string;
+  content: string;
+  date: string;
+  category: string;
+  chat_room_id: number;
+};
+
 export type AppointmentInputs = {
   title: string;
   address: { place: string; detailPlace: string };
