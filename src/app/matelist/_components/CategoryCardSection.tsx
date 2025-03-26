fd05796f156b6b0701876efd13adb52aa374c27e
K-jisu <wltn7star@naver.com>
2025-03-26 14:20:29 +0900

    🎉 Add: 카드리스트 반복되는 부분 컴포넌트 화#76

---
+++ src/app/matelist/_components/CategoryCardSection.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import UserCardList from './UserCardList';
+import { Button } from '@/ui/shadcn/button';
+
+type CategoryCardSectionProps = {
+  categories: string[];
+};
+
+const CategoryCardSection = ({ categories }: CategoryCardSectionProps) => {
+  return (
+    <>
+      {categories.map((category) => (
+        <div key={category}>
+          <div className="flex flex-row w-full pl-1 pt-10">
+            <Button variant={'label'} size={'label'}>
+              {category}
+            </Button>
+            <h3 className="text-title-sm pl-3">같이 할 파우니를 찾고있어요!</h3>
+          </div>
+          <UserCardList category={category} />
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default CategoryCardSection;
