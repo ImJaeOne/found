f3bff5856aabbbd36268749089657328266bc6e1
K-jisu <wltn7star@naver.com>
2025-03-26 22:40:57 +0900

    ✨ update : 필터링 된 유저 반환#76

--- src/services/getUsersCategories.tsx
+++ src/services/getUsersCategories.tsx
@@ -1,7 +1,13 @@
 import { UserData } from '@/types/users';
 import { supabase } from './supabaseClient';
 
-export const getUsersCategories = async (category: string) => {
+export const getUsersCategories = async ({
+  category,
+  id,
+}: {
+  category: string;
+  id?: number;
+}) => {
   const { data, error } = await supabase.rpc(
     'get_users_with_category_all_categories',
     {
@@ -13,5 +19,12 @@ export const getUsersCategories = async (category: string) => {
     throw new Error('users 데이터를 불러오지 못했습니다');
   }
 
-  return data as UserData[];
+  const users = data as UserData[];
+  const usersFingind = users.filter((user) => user.is_finding);
+  if (id) {
+    const filteredData = usersFingind.filter((item) => item.id !== id);
+
+    return filteredData as UserData[];
+  }
+  return usersFingind as UserData[];
 };
