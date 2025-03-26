021ef88fddb4ec3f9d3f459c8d755a472ef7eb39
Juha Yoon <ijooha16@gmail.com>
2025-03-27 02:45:58 +0900

    ✨ Update: 프로필 변경 시 zustand에 적용

--- src/app/mypage/_components/Appointments.tsx
+++ src/app/mypage/_components/Appointments.tsx
@@ -13,8 +13,8 @@ const Appointments = ({ params }: Props) => {
 
   const formatKoreanDateString = (dateStr: string) => {
     // "2025년 03월 26일" 형태를 "2025-03-26"로 변환
-    const parts = dateStr.split(' ');
-    if (parts.length !== 3) return dateStr; // 예상과 다른 형식일 경우 그대로 반환
+    const parts = dateStr?.split(' ');
+    if (parts?.length !== 3) return dateStr; // 예상과 다른 형식일 경우 그대로 반환
     const year = parts[0].replace('년', '').trim();
     const month = parts[1].replace('월', '').trim().padStart(2, '0');
     const day = parts[2].replace('일', '').trim().padStart(2, '0');
@@ -25,7 +25,7 @@ const Appointments = ({ params }: Props) => {
   // 선택된 옵션에 따라 약속을 필터링: upcoming은 현재 날짜 이후, previous는 이전 날짜
   const filteredAppointments = appointments?.filter((appointment) => {
     const appointmentDate = new Date(
-      formatKoreanDateString(appointment.appointments[0].date),
+      formatKoreanDateString(appointment.appointments[0]?.date),
     );
     return selected === 'upcoming'
       ? appointmentDate > now
--- src/app/mypage/_components/ProfileDialog.tsx
+++ src/app/mypage/_components/ProfileDialog.tsx
@@ -24,6 +24,7 @@ import { ImageType } from '@/types/image';
 import { Props } from '../[id]/page';
 import { useToast } from '@/hooks/useToast';
 import { MYPAGE_TOAST_MESSAGES } from '@/constants/my-page';
+import { useAuthStore } from '@/providers/AuthProvider';
 
 const ProfileDialog = ({ params }: Props) => {
   const { mutate: editProfile } = useEditProfileMutation(params.id);
@@ -39,6 +40,7 @@ const ProfileDialog = ({ params }: Props) => {
   const { data } = useProfileImageQuery(userData?.profile, {
     enabled: !!userData?.profile, // userData.profile이 있을 때만 쿼리 실행
   });
+  const setLogin = useAuthStore((state) => state.setLogin);
 
   const imageUrl = data as ImageType;
 
@@ -114,6 +116,7 @@ const ProfileDialog = ({ params }: Props) => {
       {
         onSuccess: () => {
           toast({ description: MYPAGE_TOAST_MESSAGES.EDITPROFILE });
+          setLogin({ ...defaultUserData, ...userData, profile: imageUrl });
         },
         onError: () => {
           toast({ description: MYPAGE_TOAST_MESSAGES.ERROR.EDITPROFILE });
