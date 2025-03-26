b56b890954cdd2e9d29514d1f87b998aefdf6815
ImJaeOne <dlawi0108@naver.com>
2025-03-27 04:28:22 +0900

    ✨ Update: generateMetadata 설정  #95

--- src/app/@modal/(.)appointment/[id]/page.tsx
+++ src/app/@modal/(.)appointment/[id]/page.tsx
@@ -1,5 +1,21 @@
 import { Params } from '@/types/params';
 import AppointmentModal from '../_components/AppointmentModal';
+import { createClient } from '@/services/server';
+import { fetchChatPartner } from '@/services/usersServices';
+
+export const generateMetadata = async ({ params }: Params) => {
+  const chatId = Number(params.id);
+  const serverClient = await createClient();
+  const { data } = await serverClient.auth.getUser();
+  const chatPartner = await fetchChatPartner(
+    chatId,
+    data.user?.user_metadata.id,
+  );
+  return {
+    title: `${chatPartner.nickname}님과의 약속`,
+    description: '운동 메이트를 쉽게 구하고 운동을 시작해보세요.',
+  };
+};
 
 const AppointmentModalPage = ({ params }: Params) => {
   const chatId = Number(params.id);
--- src/app/appointment/[id]/page.tsx
+++ src/app/appointment/[id]/page.tsx
@@ -1,12 +1,28 @@
 import { Params } from '@/types/params';
 import AppointmentForm from '../_components/AppointmentForm';
+import { createClient } from '@/services/server';
+import { fetchChatPartner } from '@/services/usersServices';
+
+export const generateMetadata = async ({ params }: Params) => {
+  const chatId = Number(params.id);
+  const serverClient = await createClient();
+  const { data } = await serverClient.auth.getUser();
+  const chatPartner = await fetchChatPartner(
+    chatId,
+    data.user?.user_metadata.id,
+  );
+  return {
+    title: `${chatPartner.nickname}님과의 약속`,
+    description: '운동 메이트를 쉽게 구하고 운동을 시작해보세요.',
+  };
+};
 
 const AppointmentPage = ({ params }: Params) => {
   const chatId = Number(params.id);
 
   return (
     <div className="h-[calc(100vh-56px)] p-32 bg-main2">
-      <div className="container mx-auto max-w-5xl rounded-2xl shadow-xl border border-gray-200 p-10 bg-white  w-full h-full flex items-center justify-center">
+      <div className="container mx-auto max-w-5xl rounded-2xl shadow-xl border border-gray-200 p-10 bg-white w-full flex items-center justify-center">
         <AppointmentForm chatId={chatId} />
       </div>
     </div>
