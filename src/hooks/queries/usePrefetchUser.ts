4301d41f141b9266d2b4cebd3f6a00a90bd538fd
K-jisu <wltn7star@naver.com>
2025-03-26 22:39:42 +0900

    🎉 Add: id 여부에 따른 데이터 다르게 받아오기#76

--- src/hooks/queries/usePrefetchUser.ts
+++ src/hooks/queries/usePrefetchUser.ts
@@ -2,19 +2,30 @@ import { CATEGORIES, QUERY_KEY } from '@/constants/constants';
 import { getUsersCategories } from '@/services/getUsersCategories';
 import { useQueryClient } from '@tanstack/react-query';
 
-export const usePrefetchUser = () => {
+export const usePrefetchUser = (id?: number) => {
   const queryClient = useQueryClient();
   const categoryList = Object.values(CATEGORIES);
 
   const handleHoverMateList = async () => {
-    await Promise.all(
-      categoryList.map((category) => {
-        queryClient.prefetchQuery({
-          queryKey: [QUERY_KEY.USERS, category],
-          queryFn: () => getUsersCategories(category),
-        });
-      }),
-    );
+    if (id) {
+      await Promise.all(
+        categoryList.map((category) => {
+          queryClient.prefetchQuery({
+            queryKey: [QUERY_KEY.USERS, category, id],
+            queryFn: () => getUsersCategories({ category, id }),
+          });
+        }),
+      );
+    } else {
+      await Promise.all(
+        categoryList.map((category) => {
+          queryClient.prefetchQuery({
+            queryKey: [QUERY_KEY.USERS, category],
+            queryFn: () => getUsersCategories({ category }),
+          });
+        }),
+      );
+    }
   };
 
   return handleHoverMateList;
--- src/hooks/queries/useUsersCategories.ts
+++ src/hooks/queries/useUsersCategories.ts
@@ -2,9 +2,22 @@ import { QUERY_KEY } from '@/constants/constants';
 import { getUsersCategories } from '@/services/getUsersCategories';
 import { useQuery } from '@tanstack/react-query';
 
-export const useUsersCategories = (category: string) => {
-  return useQuery({
-    queryKey: [QUERY_KEY.USERS, category],
-    queryFn: () => getUsersCategories(category),
-  });
+export const useUsersCategories = ({
+  category,
+  id,
+}: {
+  category: string;
+  id?: number;
+}) => {
+  if (id) {
+    return useQuery({
+      queryKey: [QUERY_KEY.USERS, category, id],
+      queryFn: () => getUsersCategories({ category, id }),
+    });
+  } else {
+    return useQuery({
+      queryKey: [QUERY_KEY.USERS, category],
+      queryFn: () => getUsersCategories({ category }),
+    });
+  }
 };
